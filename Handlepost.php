979344794d4386cfc328576f2ea6174313f4eb8a
Kevin-Chen1 <89753837+Kevin-Chen1@users.noreply.github.com>
2022-10-04 19:53:38 -0500

    Update Handlepost.php

--- Handlepost.php
+++ Handlepost.php
@@ -1,7 +1,7 @@
 <?php require_once("header.php"); ?>
 
 
-<form action="handlePOST.php" method="post">
+<form action="handleGET.php" method="get">
 Name: <input type="text" name="name"><br>
 Year: <input type="text" name="year"><br>
 <input type="submit">
