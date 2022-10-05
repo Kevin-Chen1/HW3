15cf20d73ca6746afdd83bd452515ae54eaf7a8a
Kevin-Chen1 <89753837+Kevin-Chen1@users.noreply.github.com>
2022-10-04 19:33:49 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,19 @@
+<?php require_once("header.php"); ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
-  <body>
-<table class="table table-striped">
+
+  <table class="table table-striped">
   <thead>
     <tr>
-      <th>course_id</th>
-      <th>prefix</th>
+      <th>Course ID</th>
+      <th>Prefix</th>
+      <th>Number</th>
+      <th>Description</th>
     </tr>
   </thead>
   <tbody>
-    <?php
+<?php
+    
 $servername = "localhost";
 $username = "kevinche_User1";
 $password = "I<3oklahoma";
@@ -28,18 +26,18 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT *";
+$sql = "SELECT course_id, prefix";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-?>
-  <tr>
-    <td><?=$row["course_id"]?></td>
-    <td><?=$row["prefix"]?></td>
-  </tr>
-<?php
+
+echo  <tr>
+    <td scope="row">' . $row["course_id"]. '</td>
+                  <td>' . $row["prefix"] .'</td>
+  <br>
+  </tr>';
   }
 } else {
   echo "0 results";
@@ -48,14 +46,5 @@ $conn->close();
 ?>
   </tbody>
     </table>
-    <h1>Hello, world!</h1>
-    <form action="Handlepost.php" method="get">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
-    
-    <a class="btn btn-primary" href="page2.html" role="button">Page 2</a>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
-</html>
+    </div>
+    </table>
